f1ba25e48ff6f2964f422928a21dce16144e54ec
TemporinSimone <90798726+TemporinSimone@users.noreply.github.com>
2023-03-23 08:49:39 +0100

    Create order.php

---
+++ Back-End/MODEL/order.php
@@ -0,0 +1,50 @@
+<?php
+
+spl_autoload_register(function ($class) {
+    require __DIR__ . "/../COMMON/$class.php";
+});
+
+set_exception_handler("errorHandler::handleException");
+set_error_handler("errorHandler::handleError");
+
+class Order
+{
+    private PDO $conn;
+    private Connect $db;
+
+    public function __construct()
+    {
+        $this->db = new Connect;
+        $this->conn = $this->db->getConnection();
+    }
+
+    public function getOrder($id) //ritorna l'ordine richiesto ricevendo in input l'id dell'ordine stesso
+    {
+        $sql = "SELECT o.id, o.id_account, o.address, o.date_order, f.name, of2.weight, o.total_price, o.status
+        from `order` o
+        inner join order_formaggyo of2 on o.id = of2.id_order
+        inner join formaggyo f on f.id = of2.id_formaggyo
+        where o.id = :id ";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function getArchiveOrder() //Ritorna tutti gli ordini.
+    {
+        $query = "SELECT o.id, o.id_account, o.address, o.date_order, f.name, of2.weight, o.total_price, o.status
+        from `order` o
+        inner join order_formaggyo of2 on o.id = of2.id_order
+        inner join formaggyo f on f.id = of2.id_formaggyo";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+?>
