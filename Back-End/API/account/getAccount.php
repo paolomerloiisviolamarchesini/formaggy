94376fe09de0bb11b0e0ef78975713969b4d3d76
TemporinSimone <90798726+TemporinSimone@users.noreply.github.com>
2023-03-27 17:24:16 +0200

    Update getAccount.php

--- Back-End/API/account/getAccount.php
+++ Back-End/API/account/getAccount.php
@@ -4,7 +4,7 @@ header("Content-type: application/json; charset=UTF-8");
 
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
 
-if (empty($parts[5])) {
+if (empty($parts[6])) {
     http_response_code(404);
     echo json_encode(["message" => "Insert a valid ID"]);
     exit();
