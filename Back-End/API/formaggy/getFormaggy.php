47935ef0eaf715cc2d9ba7f4406dd3752159c0db
AndreaZanella <90708805+AndreaZanella@users.noreply.github.com>
2023-03-31 08:38:42 +0200

    Update getFormaggy.php

--- Back-End/API/formaggy/getFormaggy.php
+++ Back-End/API/formaggy/getFormaggy.php
@@ -1,20 +1,26 @@
 <?php
+
 require __DIR__ . '/../../MODEL/formaggy.php';
+
 header("Content-type: application/json; charset=UTF-8");
+header("Content-type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin');
 
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
 
-if (empty($parts[6])) {
+if (empty($parts[5])) {
     http_response_code(404);
-    echo json_encode(["message" => "Insert a valid ID"]);
+    echo json_encode(["message" => "Inserisci un ID valido"]);
     exit();
 }
 
 $formaggy = new Formaggy();
 
-$result = $formaggy->getFormaggy($parts[6]);
-$ingredients = $formaggy->getFormaggyIngredients($parts[6]);
-$sizes = $formaggy->getFormaggySizes($parts[6]);
+$result = $formaggy->getFormaggy($parts[5]);
+$ingredients = $formaggy->getFormaggyIngredients($parts[5]);
+$sizes = $formaggy->getFormaggySizes($parts[5]);
 $formaggyArchiveFormaggy = array(
     "id" => $result["id"],
     "name" => $result["name"],
@@ -24,6 +30,7 @@ $formaggyArchiveFormaggy = array(
     "certification" => $result["certification"],
     "color" => $result["color"],
     "smell" => $result["smell"],
+    "taste" => $result["taste"],
     "expiry_date" => $result["expiry_date"],
     "kcal" => $result["kcal"],
     "fats" => $result["fats"],
@@ -41,5 +48,7 @@ if ($result != false) {
     echo json_encode($formaggyArchiveFormaggy);
 } else {
     http_response_code(400);
-    echo json_encode(["message" => "Cheese not found"]);
+    echo json_encode(["message" => "Formaggyo non trovato"]);
 }
+
+?>
