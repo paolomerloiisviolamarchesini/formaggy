40038350c1cfad1d96a0ac29716411ea1a44eb8c
AndreaZanella <90708805+AndreaZanella@users.noreply.github.com>
2023-03-31 11:55:55 +0200

    Update getOrderFormaggy.php

--- Back-End/API/order/getOrderFormaggy.php
+++ Back-End/API/order/getOrderFormaggy.php
@@ -1,10 +1,14 @@
 <?php
 require __DIR__ . '/../../MODEL/order.php';
+
 header("Content-type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin');
 
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
 
-if (empty($parts[6])) {
+if (empty($parts[5])) {
     http_response_code(404);
     echo json_encode(["message" => "Inserisci un ID valido"]);
     exit();
@@ -12,11 +16,12 @@ if (empty($parts[6])) {
 
 $order = new Order();
 
-$result = $order->getOrderFormaggy($parts[6]);
+$result = $order->getOrderFormaggy($parts[5]);
 
 if ($result != false) {
     echo json_encode($result);
 } else {
     http_response_code(400);
-    echo json_encode(["messaggio" => "Ordine non trovato"]);
+    echo json_encode(["messaggio" => "La ricerca non ha prodotto risultati"]);
 }
+?>
