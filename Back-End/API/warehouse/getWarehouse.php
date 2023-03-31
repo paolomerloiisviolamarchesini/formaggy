c23c3668996d6b8c66b39058c5c8948d73fe86ac
AndreaZanella <90708805+AndreaZanella@users.noreply.github.com>
2023-03-31 09:06:46 +0200

    Update getWarehouse.php

--- Back-End/API/warehouse/getWarehouse.php
+++ Back-End/API/warehouse/getWarehouse.php
@@ -1,22 +1,28 @@
 <?php
 require __DIR__ . '/../../MODEL/warehouse.php';
+
 header("Content-type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin');
 
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
 
 if (empty($parts[5])) {
     http_response_code(404);
-    echo json_encode(["message" => "Insert a valid ID"]);
+    echo json_encode(["message" => "Inserisci un ID valido"]);
     exit();
 }
 
 $warehouse = new Warehouse();
 
-$result = $warehouse->getWarehouse($parts[6]);
+$result = $warehouse->getWarehouse($parts[5]);
 
 if ($result != false) {
     echo json_encode($result);
 } else {
     http_response_code(400);
-    echo json_encode(["message" => "Warehouse not found"]);
+    echo json_encode(["message" => "Magazzino non trovato"]);
 }
+
+?>
