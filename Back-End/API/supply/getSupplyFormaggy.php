4179eb9e7670baec74bccb92046a64241c5aa30b
AndreaZanella <90708805+AndreaZanella@users.noreply.github.com>
2023-03-31 11:54:54 +0200

    Update getSupplyFormaggy.php

--- Back-End/API/supply/getSupplyFormaggy.php
+++ Back-End/API/supply/getSupplyFormaggy.php
@@ -1,22 +1,29 @@
 <?php
 require __DIR__ . '/../../MODEL/supply.php';
+
 header("Content-type: application/json; charset=UTF-8");
+header("Content-type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin');
 
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
 
-if (empty($parts[6])) {
+if (empty($parts[5])) {
     http_response_code(404);
-    echo json_encode(["message" => "Insert a valid ID"]);
+    echo json_encode(["message" => "Inserisci un ID valido"]);
     exit();
 }
 
 $supply = new Supply();
 
-$result = $supply->getSupplyFormaggy($parts[6]);
+$result = $supply->getSupplyFormaggy($parts[5]);
 
 if ($result != false) {
     echo json_encode($result);
 } else {
     http_response_code(400);
-    echo json_encode(["message" => "cheese not found"]);
+    echo json_encode(["message" => "La ricerca non ha prodotto risultati"]);
 }
+
+?>
