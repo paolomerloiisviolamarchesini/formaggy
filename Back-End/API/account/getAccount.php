c2e67177ff3f7b4cf61017dd49d90febf7296756
AndreaZanella <90708805+AndreaZanella@users.noreply.github.com>
2023-03-31 08:21:03 +0200

    Update getAccount.php

--- Back-End/API/account/getAccount.php
+++ Back-End/API/account/getAccount.php
@@ -1,22 +1,30 @@
 <?php
+
 require __DIR__ . '/../../MODEL/account.php';
+
 header("Content-type: application/json; charset=UTF-8");
+header("Content-type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin');
 
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
 
-if (empty($parts[6])) {
+if (empty($parts[5])) {
     http_response_code(404);
-    echo json_encode(["message" => "Insert a valid ID"]);
+    echo json_encode(["message" => "Inserisci un id valido"]);
     exit();
 }
 
 $account = new Account();
 
-$result = $account->getAccount($parts[6]);
+$result = $account->getAccount($parts[5]);
 
 if ($result != false) {
     echo json_encode($result);
 } else {
     http_response_code(400);
-    echo json_encode(["message" => "Account not found"]);
+    echo json_encode(["message" => "Account non trovato"]);
 }
+
+?>
