8e3b272675a11bc144a8ac6c3a7962e0889c2a1a
Formenton Matteo <90769483+Formenton04@users.noreply.github.com>
2023-03-31 12:30:18 +0200

    Update account.php

--- Back-End/MODEL/account.php
+++ Back-End/MODEL/account.php
@@ -43,5 +43,127 @@ class Account
         
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+        public function login($email, $password)
+    {
+        $sql = "SELECT a.id
+        FROM account a 
+        WHERE a.email =:email and a.secret =:password";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+        $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+        $stmt->execute();
+
+        return $stmt->rowCount();
+    }
+        public function addAccount($email, $username, $secret, $permits)
+{
+    // Controllo se ci sono già altri utenti con la stessa mail
+    $sql = "SELECT a.id
+    FROM account a
+    WHERE a.email = :email";
+
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+
+    $stmt->execute();
+
+    // Creo una variabile per contenere l'id dell'utente creato
+    $stmt->fetch(PDO::FETCH_ASSOC);
+    
+
+    if ($stmt->rowCount() == 0)
+    {
+        // Aggiungo l'utente nella tabella user
+        $sql = "INSERT INTO account 
+        (email, username, secret, permits)
+        VALUES (:email, :username, :secret, :permits)";
+        
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+        $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+        $stmt->bindValue(':secret', $secret, PDO::PARAM_STR);
+        $stmt->bindValue(':permits', $permits, PDO::PARAM_INT);
+        
+        $stmt->execute();
+        return ["message" => "Utente creato con successo"];
+    }
+    else
+    {
+        return ["message" => "Utente già esistente"];
+    }
+}
+public function modifyPassword($id_account,$new_password)
+{
+    $sql=" UPDATE account  
+           SET secret = :new_password
+           WHERE id=:id_account";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bindValue(':id_account', $id_account, PDO::PARAM_INT);
+    $stmt->bindValue(':new_password', $new_password, PDO::PARAM_INT);
+    $stmt->execute();
+
+    return $stmt->rowCount();
+
+}
+
+public function modifyAccount($id_account,$email,$username)
+{
+    $sql="UPDATE account
+    SET email =:email
+    WHERE id=:id_account";
+
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bindValue(':id_account', $id_account, PDO::PARAM_INT);
+    $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+    $stmt->execute();
+    $cnt=$stmt->rowCount();
+
+
+    $sql="UPDATE account
+    SET username =:username
+    WHERE id=:id_account";
+
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bindValue(':id_account', $id_account, PDO::PARAM_INT);
+    $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+    $stmt->execute();
+
+    $cnt+=$stmt->rowCount();
+
+    return $cnt;
+}
+
+public function deleteAccount($id_account)
+{
+
+    $sql="UPDATE account
+    SET permits =-1 
+    WHERE id=:id_account";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bindValue(':id_account', $id_account, PDO::PARAM_INT);
+    $stmt->execute();
+
+    if ($stmt->rowCount() == 1)
+    return 1;
+    else 
+    return 0;
+}
+
+public function ActivateUser($id_account,$permits)
+{
+    $sql="UPDATE account
+    SET permits =:permits 
+    WHERE id=:id_account";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bindValue(':id_account', $id_account, PDO::PARAM_INT);
+    $stmt->bindValue(':permits', $permits, PDO::PARAM_INT);
+    $stmt->execute();
+
+    if ($stmt->rowCount() == 1)
+    return 1;
+    else 
+    return 0;   
+}
 }
 ?>
