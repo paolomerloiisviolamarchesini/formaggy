8f79f6e79102a23ce8ba690a525f7b0c1f2cf3ef
AndreaZanella <90708805+AndreaZanella@users.noreply.github.com>
2023-03-31 08:46:45 +0200

    Update getArchiveIngredient.php

--- Back-End/API/ingredient/getArchiveIngredient.php
+++ Back-End/API/ingredient/getArchiveIngredient.php
@@ -4,10 +4,17 @@ header("Content-type: application/json; charset=UTF-8");
 
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
 
+header("Content-type: application/json; charset=UTF-8");
+header("Content-type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin');
+
 $ingredient = new Ingredient();
 
 $result = $ingredient->getArchiveIngredient();
 $ingredientArchiveIngredients = array();
+
 for ($i = 0; $i < (count($result)); $i++) {
 $ingredientArchiveIngredient = array(
     "id" => $result[$i]["id"],
@@ -19,8 +26,10 @@ array_push($ingredientArchiveIngredients, $ingredientArchiveIngredient);
 
 if (empty($ingredientArchiveIngredients)) {
     echo json_encode(404);
-    echo json_encode(["message" => "Can't find any ingredient"]);
+    echo json_encode(["message" => "La ricerca non ha prodotto risultati"]);
 } else {
     http_response_code(200);
     echo json_encode($ingredientArchiveIngredients);
 }
+
+?>
