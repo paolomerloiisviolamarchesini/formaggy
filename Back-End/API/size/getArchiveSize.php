eda85b2df9c894746d93a03c2baaff95bc666d63
AndreaZanella <90708805+AndreaZanella@users.noreply.github.com>
2023-03-31 11:52:16 +0200

    Update getArchiveSize.php

--- Back-End/API/size/getArchiveSize.php
+++ Back-End/API/size/getArchiveSize.php
@@ -1,6 +1,11 @@
 <?php
 require __DIR__ . '/../../MODEL/size.php';
+
 header("Content-type: application/json; charset=UTF-8");
+header("Content-type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin');
 
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
 
@@ -18,8 +23,9 @@ array_push($sizeArchivesizes, $sizeArchivesize);
 
 if (empty($sizeArchivesizes)) {
     echo json_encode(404);
-    echo json_encode(["messaggio" => "Nessuna dimensione è stata trovata"]);
+    echo json_encode(["messaggio" => "La ricerca non ha prodotto risultati"]);
 } else {
     http_response_code(200);
     echo json_encode($sizeArchivesizes);
 }
+?>
