1e1581ae05320d5c808dc813ccb6426948c53716
AndreaZanella <90708805+AndreaZanella@users.noreply.github.com>
2023-03-31 09:05:18 +0200

    Update getArchiveWarehouse.php

--- Back-End/API/warehouse/getArchiveWarehouse.php
+++ Back-End/API/warehouse/getArchiveWarehouse.php
@@ -1,6 +1,10 @@
 <?php
 require __DIR__ . '/../../MODEL/warehouse.php';
+
 header("Content-type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin');
 
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
 
@@ -18,8 +22,10 @@ array_push($warehouseArchiveWarehouses, $warehouseArchiveWarehouse);
 
 if (empty($warehouseArchiveWarehouses)) {
     echo json_encode(404);
-    echo json_encode(["message" => "Can't find any warehouse"]);
+    echo json_encode(["message" => "La ricerca non ha prodotto risultati"]);
 } else {
     http_response_code(200);
     echo json_encode($warehouseArchiveWarehouses);
 }
+
+?>
