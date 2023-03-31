d6eb2c458d88078bed19cacac6501607ff124079
AndreaZanella <90708805+AndreaZanella@users.noreply.github.com>
2023-03-31 09:07:58 +0200

    Update getWarehouseFormaggy.php

--- Back-End/API/warehouse/getWarehouseFormaggy.php
+++ Back-End/API/warehouse/getWarehouseFormaggy.php
@@ -4,15 +4,15 @@ header("Content-type: application/json; charset=UTF-8");
 
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
 
-if (empty($parts[6])) {
+if (empty($parts[5])) {
     http_response_code(404);
-    echo json_encode(["message" => "Insert a valid ID"]);
+    echo json_encode(["message" => "Inserisci un ID valido"]);
     exit();
 }
 
 $warehouseformaggy = new Warehouse();
 
-$result = $warehouseformaggy->getWarehouseFormaggy($parts[6]);
+$result = $warehouseformaggy->getWarehouseFormaggy($parts[5]);
 $warehouseArchiveFormaggys = array();
 for ($i = 0; $i < (count($result)); $i++) {
 $warehouseArchiveFormaggy = array(
@@ -25,8 +25,10 @@ array_push($warehouseArchiveFormaggys, $warehouseArchiveFormaggy);
 
 if (empty($warehouseArchiveFormaggys)) {
     echo json_encode(404);
-    echo json_encode(["message" => "there isn't cheese in warehouse"]);
+    echo json_encode(["message" => "La ricerca non ha prodotto risultati"]);
 } else {
     http_response_code(200);
     echo json_encode($warehouseArchiveFormaggys);
 }
+
+?>
