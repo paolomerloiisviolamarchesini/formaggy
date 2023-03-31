34a6b51dcf806048816fe17903d927ac5119d4f7
Formenton Matteo <90769483+Formenton04@users.noreply.github.com>
2023-03-31 12:56:02 +0200

    Create deleteSupply.php

---
+++ Back-End/API/supply/deleteSupply.php
@@ -0,0 +1,36 @@
+<?php
+
+spl_autoload_register(function ($class) {
+    require __DIR__ . "/../../COMMON/$class.php";
+});
+
+require __DIR__ . '/../../MODEL/supply.php';
+header("Content-type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Origin');
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header('HTTP/1.1 200 OK');
+}
+
+$data = json_decode(file_get_contents("php://input"));
+
+
+if (empty($data->id_supply)) {
+    http_response_code(400);
+    echo json_encode(["message" => "Fill every field"]);
+    die();
+}
+
+$supply = new supply();
+
+if ($supply->deleteSupply($data->id_supply)==1) {
+    http_response_code(200);
+    echo json_encode(["message" => "Supply eliminata con successo"]);
+}
+else
+{
+    http_response_code(400);
+    echo json_encode(["message" => "Problemi con l'eliminazione della supply"]);
+}
